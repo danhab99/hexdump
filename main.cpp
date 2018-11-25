a07a5afd8db7225abc93f02b923f0e50cfbd30dd
Dan Habot <dan.habot@gmail.com>
2018-11-24 19:38:36 -0500

    Sped up program

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <iomanip>
 #include "argparse/argparse.hpp"
 
@@ -18,29 +19,32 @@ int main(int argc, const char** argv) {
 	ifile.open(parser.retrieve<string>("file"), ios::in | ios::binary);
 
 	if (ifile.is_open()) {
+		stringstream output;
 		for (int i = 0; i < 9; ++i)
-			cout << " ";
+			output << " ";
 
 		for (int i = 0; i < column; ++i)
-			cout << setfill('0') << setw(2) << hex << i << ' ';
+			output << setfill('0') << setw(2) << hex << i << ' ';
 
-		cout << endl;
+		output << endl;
 
 		int line = 0;
 		while (!ifile.eof()) {
 			char buffer[column] = { 0 };
 			ifile.read(buffer, column);
 
-			cout << setfill('0') << setw(8) << hex << line << ' ';
+			output << setfill('0') << setw(8) << hex << line << ' ';
 			line += column;
 
 			for (int i = 0; i < column; ++i) {
-				cout << setfill('0') << setw(2) << hex << (int)(uint8_t)buffer[i] << ' ';
+				output << setfill('0') << setw(2) << hex << (int)(uint8_t)buffer[i] << ' ';
 			}
 
-			cout << endl;
+			output << endl;
 		}
 
+		ifile.close();
+		cout << output.str();
 		return 0;
 	}
 	else {
@@ -48,5 +52,5 @@ int main(int argc, const char** argv) {
 		return 1;
 	}
 
-	ifile.close();
+	
 }
